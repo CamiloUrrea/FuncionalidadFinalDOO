8ac52c3d429a124a545b987fcfdf2eccfcf94793
Tomas Gomez Cifuentes <tomicifuentesposada@gmail.com>
2025-05-26 23:59:46 -0500

    Utilitario de texto actualizado

--- back-end/src/main/java/co/edu/uco/backend/crosscutting/utilitarios/UtilTexto.java
+++ back-end/src/main/java/co/edu/uco/backend/crosscutting/utilitarios/UtilTexto.java
@@ -1,11 +1,29 @@
 package co.edu.uco.backend.crosscutting.utilitarios;
 
+import java.util.regex.Pattern;
+
 public final class UtilTexto {
 
     private static UtilTexto instancia = new UtilTexto();
+    private static final String PATRON_SOLO_LETRAS_ESPACIOS = "^[A-Za-zÁÉÍÓÚáéíóúÑñ ]+$";
     public final static String VACIO = "";
+    private static final Pattern EMAIL_PATTERN = Pattern.compile(
+            "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$"
+    );
+
+    private static final Pattern PATRON_CONTRASENA = Pattern.compile(
+            "^(?=.[a-z])(?=.[A-Z])(?=.\\d)(?=.[^A-Za-z0-9]).{8,}$"
+    );
 
     private UtilTexto() {}
+
+    public boolean patronEsValido(final String valor, final String patron) {
+        return obtenerValorDefecto(valor).matches(obtenerValorDefecto(patron));
+    }
+
+    public boolean contieneSoloLetrasEspacios(final String valor) {
+        return patronEsValido(quitarEspaciosEnBlancoInicioFin(valor), PATRON_SOLO_LETRAS_ESPACIOS);
+    }
 
     public static UtilTexto getInstance() {
         return instancia;
@@ -13,6 +31,11 @@ public final class UtilTexto {
 
     public Boolean esNula(final String valor) {
         return UtilObjeto.getInstance().esNulo(valor);
+    }
+
+    public boolean esVacio(final String valor) {
+
+        return VACIO.equals(quitarEspaciosEnBlancoInicioFin(valor));
     }
 
     public String obtenerValorDefecto() {
@@ -30,4 +53,29 @@ public final class UtilTexto {
     public String quitarEspaciosEnBlancoInicioFin(final String valor) {
         return obtenerValorDefecto(valor).trim();
     }
+
+    public  boolean esValorDefecto(final String valor) {
+        return obtenerValorDefecto(valor).equals(obtenerValorDefecto());
+    }
+
+    public boolean longitudValida(final String valor, int min, int max) {
+        String limpio = quitarEspaciosEnBlancoInicioFin(valor);      // ya maneja null → ""
+        int len = limpio.length();
+        return len >= min && len <= max;
+    }
+
+
+    public boolean esEmailValido(String email) {
+        if (esVacio(email)) {
+            return false;
+        }
+        return EMAIL_PATTERN.matcher(email.trim()).matches();
+    }
+
+
+    public boolean esContrasenaValida(String contrasena) {
+        if (esVacio(contrasena)) return false;
+        return PATRON_CONTRASENA.matcher(contrasena.trim()).matches();
+    }
+
 }
