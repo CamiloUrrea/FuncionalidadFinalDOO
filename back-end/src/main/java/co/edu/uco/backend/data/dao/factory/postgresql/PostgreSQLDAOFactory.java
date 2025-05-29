2baf3dbb8e2914d76d3ca79cce3777cfcdfe265c
Tomas Gomez Cifuentes <tomicifuentesposada@gmail.com>
2025-05-29 15:39:28 -0500

    front conectado

--- back-end/src/main/java/co/edu/uco/backend/data/dao/factory/postgresql/PostgreSQLDAOFactory.java
+++ back-end/src/main/java/co/edu/uco/backend/data/dao/factory/postgresql/PostgreSQLDAOFactory.java
@@ -61,7 +61,7 @@ public class PostgreSQLDAOFactory extends DAOFactory {
         var servidor = "localhost:5432";
 
         try {
-            conexion=DriverManager.getConnection("jdbc:postgresql://" + servidor + "/" + baseDatos, "postgres", "S18.ldserv");
+            conexion=DriverManager.getConnection("jdbc:postgresql://" + servidor + "/" + baseDatos, "postgres", "3104136215apa");
             connexionEstaAbierta = true;
         } catch (SQLException exception) {
             var mensajeTecnico = "Se presentó una SQLException tratando de obtener la conexión con la base de datos "
